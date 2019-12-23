80ca6b1237d5541e69a9d9266dd041541a3e73a3
RideWindX <ridewindx@163.com>
2019-12-23 15:38:32 +0800

    Add go.mod and extra.go

---
+++ extra.go
@@ -0,0 +1,10 @@
+package color
+
+var SBlac = New(FgBlack).SprintFunc()
+var SRed = New(FgRed).SprintFunc()
+var SGreen = New(FgGreen).SprintFunc()
+var SYellow = New(FgYellow).SprintFunc()
+var SBlue = New(FgBlue).SprintFunc()
+var SMagenta = New(FgMagenta).SprintFunc()
+var SCyan = New(FgCyan).SprintFunc()
+var SWhite = New(FgWhite).SprintFunc()
